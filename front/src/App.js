4cfd8eb87477f23dda1913ea7dd9ebb652bcdf26
Jjin SoL <parkleydondon@yahoo.com>
2021-07-28 17:08:44 -0700

    Repackaging using react-query. Added spinner

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,22 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { QueryClientProvider, QueryClient } from "react-query"
 import Navbar from "./components/layout/Navbar"
 import Home from "./pages/Home"
 import Product from "./pages/Product"
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/products/:productId" component={Product} />
-      </Switch>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/products/:productId" component={Product} />
+        </Switch>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- front/src/components/products/ProductList.js
+++ front/src/components/products/ProductList.js
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react"
+import { useQuery } from "react-query"
 import axios from "axios"
 import ProductItem from "./ProductItem"
+import Spinner from "../tools/Spinner"
 
 const ProductList = () => {
-  const [products, setProducts] = useState([])
-  useEffect(() => {
-    axios
-      .get("/api/products")
-      .then(res => res.data.products)
-      .then(products => setProducts(products))
-  }, [])
+  // const [products, setProducts] = useState([])
+  // useEffect(() => {
+  //   axios
+  //     .get("/api/products")
+  //     .then(res => res.data.products)
+  //     .then(products => setProducts(products))
+  // }, [])
+
+  const { data: products, isLoading } = useQuery("Products", () => axios("/api/products").then(res => res.data.products))
+
+  if (isLoading) return <Spinner />
 
   return (
     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
