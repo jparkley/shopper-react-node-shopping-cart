86b7329ac6e28e56831a0c5f9be43ab803519faa
Jjin SoL <parkleydondon@yahoo.com>
2021-07-31 00:11:04 -0700

    Added env file setup.  Added Stripe checkout(basic)

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,27 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { QueryClientProvider, QueryClient } from "react-query"
+import { loadStripe } from "@stripe/stripe-js"
+import { CartProvider } from "use-shopping-cart"
 import Navbar from "./components/layout/Navbar"
 import Home from "./pages/Home"
 import Product from "./pages/Product"
 
 const queryClient = new QueryClient()
+const STRIPE_API_P = process.env.REACT_APP_STRIPE_API_P
+const stripePromise = loadStripe(STRIPE_API_P)
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/products/:productId" component={Product} />
-        </Switch>
-      </BrowserRouter>
+      <CartProvider mode="checkout-session" stripe={stripePromise} currency="USD">
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/products/:productId" component={Product} />
+          </Switch>
+        </BrowserRouter>
+      </CartProvider>
     </QueryClientProvider>
   )
 }
