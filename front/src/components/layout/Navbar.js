d2bf0cb6dd0bc643e67218b17b0265da7522ef1a
Jjin SoL <parkleydondon@yahoo.com>
2021-08-02 23:42:20 -0700

    Updated nav menu

--- front/src/components/layout/Navbar.js
+++ front/src/components/layout/Navbar.js
@@ -1,10 +1,10 @@
-import React from "react"
+import { Collapse } from "bootstrap"
 import logo from "./shopping.svg"
 
 const Navbar = () => {
   return (
-    <div className="navbar navbar-light bg-light shadow-sm">
-      <div className="container d-flex justify-content-between">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
         <a href="/" className="navbar-brand d-flex align-items-center">
           <div className="d-flex justify-content-between">
             <img src={logo} width="24px" />
@@ -13,8 +13,31 @@ const Navbar = () => {
             </div>
           </div>
         </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link active" aria-current="page" href="#">
+              Special
+            </a>
+            <a className="nav-link" href="#">
+              FAQ
+            </a>
+            <a className="nav-link" href="#">
+              About
+            </a>
+          </div>
+        </div>
+
+        <form className="d-flex">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
       </div>
-    </div>
+    </nav>
   )
 }
 
