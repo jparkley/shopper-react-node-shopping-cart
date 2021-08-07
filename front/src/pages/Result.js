e33ae919eb5f59f39f0c3700e3a52944ccb74ff6
Jjin SoL <parkleydondon@yahoo.com>
2021-08-07 14:28:03 -0700

    Added redirect page after payment

--- front/src/pages/Result.js
+++ front/src/pages/Result.js
@@ -1,7 +1,42 @@
-import React from "react"
+import axios from "axios"
+import { useQuery } from "react-query"
+import { useLocation } from "react-router-dom"
+import { formatCurrencyString } from "use-shopping-cart"
+import Spinner from "../components/utils/Spinner"
+
+function useQueryString() {
+  return new URLSearchParams(useLocation().search)
+}
 
 const Result = () => {
-  return <div></div>
+  const queryString = useQueryString()
+  const sessionId = queryString.get("session_id")
+
+  const { data, isLoading, isError } = useQuery("Result", () => (sessionId ? axios(`/api/checkout-sessions/${sessionId}`).then(res => res.data) : null))
+
+  if (isLoading) return <Spinner />
+  if (!data && !isLoading) return <div className="display-warning">No payment found</div>
+  if (isError) return <div className="display-danger">Error in result page</div>
+
+  const total = formatCurrencyString({
+    value: data.amount_total,
+    currency: data.currency,
+    language: navigator.language
+  })
+
+  return (
+    <div className="container">
+      <main role="main" className="inner cover text-center">
+        {/* Add image here */}
+        <h1 className="cover-heading mt-10">Payment Accepted.</h1>
+        <p className="lead">Below is your payment summary. The items will be shipped to you soon.</p>
+        <p className="lead">
+          <h4 className="">Order Total: {total} </h4>
+          <h4 className="">Email:{data.customer_details.email} </h4>
+        </p>
+      </main>
+    </div>
+  )
 }
 
 export default Result
--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ module.exports = function getRoutes() {
   router.get("/products", getProducts)
   router.get("/products/:productId", getProduct)
   router.post("/checkout-sessions", createCheckoutSession)
+  router.get("/checkout-sessions/:sessionId", getCheckoutSession)
   return router
 }
 
@@ -60,6 +61,19 @@ async function createCheckoutSession(req, res) {
     res.status(200).json(checkoutSession)
   } catch (error) {
     console.log(error)
+    res.status(500).json({ statusCode: 500, message: error.message })
+  }
+}
+
+async function getCheckoutSession(req, res) {
+  const { sessionId } = req.params
+  try {
+    if (!sessionId.startsWith("cs_")) {
+      throw Error("Incorrect checkout session id")
+    }
+    const checkout_session = await stripe.checkout.sessions.retrieve(sessionId, { expand: ["payment_intent"] })
+    res.status(200).json(checkout_session)
+  } catch (error) {
     res.status(500).json({ statusCode: 500, message: error.message })
   }
 }
