c1bdfde4a3b459e5555a7fe86434fd6fb795b151
Jjin SoL <parkleydondon@yahoo.com>
2021-08-06 23:25:48 -0700

    Updated cart modal design

--- front/src/components/checkout/CartItem.js
+++ front/src/components/checkout/CartItem.js
@@ -7,24 +7,23 @@ const CartItem = ({ item }) => {
     setItemQuantity(item.id, Number(e.target.value))
   }
 
+  // NEED TO WORK ON MODAL SIZE CHANGE
   return (
     <>
-      <div className="modal-body w-100">
-        <div className="d-flex align-items-center mt-3">
-          <img className="img-tiny me-4" src={`${process.env.PUBLIC_URL}/images/products/${item.image}`} />
-          <div>
-            <span>
-              {item.name} ({item.sku})
-            </span>
-            <p className="text-start mt-1">
-              <strong>
-                {formatPrice(item)} x {item.quantity}
-              </strong>
-            </p>
-          </div>
-          <div className="ms-auto text-end">
-            <input className="text-end form-control input-quantity" type="number" value={item.quantity} min={0} onChange={handleSetItemQuantity} />
-          </div>
+      <div className="d-flex align-items-center mt-3">
+        <img className="img-tiny me-4" src={`${process.env.PUBLIC_URL}/images/products/${item.image}`} />
+        <div>
+          <span>
+            {item.name} ({item.sku})
+          </span>
+          <p className="text-start mt-1">
+            <strong>
+              {formatPrice(item)} x {item.quantity}
+            </strong>
+          </p>
+        </div>
+        <div className="ms-auto text-end">
+          <input className="text-end form-control input-quantity" type="number" value={item.quantity} min={0} onChange={handleSetItemQuantity} />
         </div>
       </div>
     </>
--- front/src/components/checkout/CartModal.js
+++ front/src/components/checkout/CartModal.js
@@ -10,26 +10,34 @@ const CartModal = ({ isOpen, toggleModal }) => {
 
   return (
     <Modal isOpen={isOpen} onRequestClose={toggleModal} contentLabel="Cart Modal" closeTimeoutMS={500} className="cart-modal" overlayClassName="cart-overlay">
-      <div className="modal-lg d-flex d-inline-flex align-items center">
-        <span className="me-3">Cart Summary</span>
-        <span>
-          <strong>
-            {formattedTotalPrice} ({cartCount} Items)
-          </strong>
-        </span>
-      </div>
-      <div className="row">
-        {cartItems.map(item => (
-          <CartItem key={item.id} item={item} />
-        ))}
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-primary text-dark">
-          <strong>Checkout</strong>
-        </button>
-        <button type="button" className="btn btn-primary text-dark" data-bs-dismiss="modal" onClick={toggleModal}>
-          Keep Shopping
-        </button>
+      <div className="">
+        <div className="modal-dialog modal-lg d-flex d-inline-flex align-items center">
+          <div className="modal-content">
+            <div className="modal-header">
+              <span className="modal-title me-3">Cart Summary</span>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <span>
+                <strong>
+                  {formattedTotalPrice} ({cartCount} Items)
+                </strong>
+              </span>
+            </div>
+
+            <div className="modal-body">
+              {cartItems.map(item => (
+                <CartItem key={item.id} item={item} />
+              ))}
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-primary text-dark">
+                <strong>Checkout</strong>
+              </button>
+              <button type="button" className="btn btn-primary text-dark" data-bs-dismiss="modal" onClick={toggleModal}>
+                Keep Shopping
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </Modal>
   )
