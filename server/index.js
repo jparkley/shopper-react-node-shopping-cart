7c8ba849c277a509566cdafc002746f068b58cf9
Jjin SoL <parkleydondon@yahoo.com>
2021-08-08 01:35:52 -0700

    Updated env variables for deployment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
 const getRoutes = require("./routes")
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 80
 
 const app = express()
 
